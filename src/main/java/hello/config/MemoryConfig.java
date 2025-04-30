6a59259614c90df51f1bd7116b2bd4bd8bda670f
Teo_You <teoyou881@gmail.com>
2025-04-29 22:49:15 -0400

    see how @conditional works

--- src/main/java/hello/config/MemoryConfig.java
+++ src/main/java/hello/config/MemoryConfig.java
@@ -2,10 +2,15 @@ package hello.config;
 
 import memory.MemoryController;
 import memory.MemoryFinder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+// @Conditional(MemoryCondition.class) // 추가
+//@ConditionalOnProperty` 도 우리가 만든 것과 동일하게 내부에는 `@Conditional` 을 사용한다.
+// 그리고 그 안에 `Condition` 인터페이스를 구현한' OnPropertyCondition` 를 가지고 있다.
+@ConditionalOnProperty(name = "memory", havingValue = "on") // 추가
 public class MemoryConfig {
 
   @Bean
